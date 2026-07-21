edfc5d3763a1ea766d15e772a41be736803bc792
Meka Claude <mekaotele@gmail.com>
2026-07-21 15:48:03 +0100

    New build

--- src/fontra/_version.py
+++ src/fontra/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '2026.7.16.2'
-__version_tuple__ = version_tuple = (2026, 7, 16, 2)
+__version__ = version = '2026.7.20.19'
+__version_tuple__ = version_tuple = (2026, 7, 20, 19)
 
 __commit_id__ = commit_id = None
